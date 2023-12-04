4c2a390272949a336cec7c2309e564e7fc5db579
Zachary S <zasample18+github@gmail.com>
2023-12-03 23:22:53 -0600

    Day 4 pre-parse.

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -33,8 +33,8 @@ impl Card {
     }
 }
 
-fn part_1(input: &str) -> u64 {
-    let cards = input
+fn parse(input: &str) -> Vec<Card> {
+    input
         .lines()
         .map(str::trim)
         .map(|line| {
@@ -53,31 +53,13 @@ fn part_1(input: &str) -> u64 {
             have.sort_unstable();
             Card { idx, winning, have }
         })
-        .collect_vec();
-    cards.iter().map(|card| card.point_value()).sum()
+        .collect_vec()
 }
 
-fn part_2(input: &str) -> usize {
-    let cards = input
-        .lines()
-        .map(str::trim)
-        .map(|line| {
-            let (idx, rest) = line["Card ".len()..].split_once(':').unwrap();
-            let (winning, have) = rest.split_once('|').unwrap();
-            let idx = idx.trim().parse().unwrap();
-            let mut winning = winning
-                .split_whitespace()
-                .map(|value| value.parse().unwrap())
-                .collect_vec();
-            winning.sort_unstable();
-            let mut have = have
-                .split_whitespace()
-                .map(|value| value.parse().unwrap())
-                .collect_vec();
-            have.sort_unstable();
-            Card { idx, winning, have }
-        })
-        .collect_vec();
+fn part_1(cards: &Vec<Card>) -> u64 {
+    cards.iter().map(|card| card.point_value()).sum()
+}
+fn part_2(cards: &Vec<Card>) -> u64 {
     let mut cards_won = [0; 11];
     let mut total_cards = 0;
     for card in cards {
@@ -97,9 +79,12 @@ fn part_2(input: &str) -> usize {
 fn main() {
     let session = std::fs::read_to_string(".session.txt").unwrap();
     let session = session.trim();
+    let mut both = PreParsed::new(parse, part_1, part_2);
+    let part_2 = both.part_2();
     if let Err(error) = aoc_magic!(session, 2023:4:2, part_2) {
         eprintln!("Part 2 failed: {error:?}");
     }
+    let part_1 = both.part_1();
     if let Err(error) = aoc_magic!(session, 2023:4:1, part_1) {
         eprintln!("Part 1 failed: {error:?}");
     }
@@ -113,6 +98,9 @@ Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
 Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
-    assert_eq!(part_1(input), 13);
-    assert_eq!(part_2(input), 30);
+    let mut both = PreParsed::new(parse, part_1, part_2);
+    let part_1 = both.part_1();
+    assert_eq!(part_1(input), &13);
+    let part_2 = both.part_2();
+    assert_eq!(part_2(input), &30);
 }
