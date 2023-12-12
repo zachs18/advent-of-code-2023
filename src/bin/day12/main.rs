c1f4980e1d727134cf578226482645ea20a27662
Zachary S <zasample18+github@gmail.com>
2023-12-12 00:28:23 -0600

    Day 12 pre-parsed and cleanup.

--- src/bin/day12/main.rs
+++ src/bin/day12/main.rs
@@ -4,32 +4,63 @@ use std::collections::HashMap;
 use aoc_2023::*;
 use aoc_driver::*;
 use itertools::Itertools;
-use regex::Regex;
 use zachs18_stdx::*;
 
-fn solve(
-    known: &[Option<bool>],
-    expected: &[usize],
+struct Row {
+    known_cells: Vec<Option<bool>>,
+    expected_chunks: Vec<usize>,
+}
+
+fn parse(input: &str) -> Vec<Row> {
+    input
+        .lines()
+        .map(str::trim)
+        .map(|line| {
+            let (known_cells, expected_chunks) = line.split_once(' ').unwrap();
+            let known_cells: Vec<Option<bool>> = known_cells
+                .chars()
+                .map(|c| match c {
+                    '.' => Some(false),
+                    '?' => None,
+                    '#' => Some(true),
+                    _ => unreachable!(),
+                })
+                .collect();
+            let expected_chunks = expected_chunks
+                .split(',')
+                .map(|field| field.parse::<usize>().unwrap())
+                .collect_vec();
+            Row {
+                known_cells,
+                expected_chunks,
+            }
+        })
+        .collect()
+}
+
+fn solve_row(
+    known_cells: &[Option<bool>],
+    expected_chunks: &[usize],
     known_start_idx: usize,
     expected_start_idx: usize,
     cache: &mut HashMap<(usize, usize), usize>,
 ) -> usize {
     if let Some(&result) = cache.get(&(known_start_idx, expected_start_idx)) {
         return result;
-    } else if expected_start_idx == expected.len() {
+    } else if expected_start_idx == expected_chunks.len() {
         // No more sections to place, just check if all remaining cells are empty.
-        let valid = known[known_start_idx..]
+        let valid = known_cells[known_start_idx..]
             .iter()
             .all(|cell| cell.is_none_or(|cell| !cell));
         cache.insert((known_start_idx, expected_start_idx), valid as usize);
         return valid as usize;
-    } else if known_start_idx == known.len() {
+    } else if known_start_idx == known_cells.len() {
         // Trying to fit expected sections into no space
         return 0;
-    } else if expected_start_idx == expected.len() - 1 {
-        let next_expected = expected[expected_start_idx];
+    } else if expected_start_idx == expected_chunks.len() - 1 {
+        let next_expected = expected_chunks[expected_start_idx];
         let mut acc = 0;
-        let mut section = &known[known_start_idx..];
+        let mut section = &known_cells[known_start_idx..];
         while section.len() >= next_expected {
             if section[..next_expected]
                 .iter()
@@ -49,22 +80,21 @@ fn solve(
         return acc;
     }
 
-    let next_expected = expected[expected_start_idx];
+    let next_expected = expected_chunks[expected_start_idx];
     let next_expected_start_idx = expected_start_idx + 1;
 
     let mut acc = 0;
     let mut this_known_start_idx = known_start_idx;
-    while known.len() - this_known_start_idx > next_expected {
-        let section = &known[this_known_start_idx..];
-        if section.len() == next_expected {}
+    while known_cells.len() - this_known_start_idx > next_expected {
+        let section = &known_cells[this_known_start_idx..];
         if section[..next_expected]
             .iter()
             .all(|cell| cell.is_none_or(|cell| cell))
             && section[next_expected].is_none_or(|cell| !cell)
         {
-            acc += solve(
-                known,
-                expected,
+            acc += solve_row(
+                known_cells,
+                expected_chunks,
                 this_known_start_idx + next_expected + 1,
                 next_expected_start_idx,
                 cache,
@@ -79,56 +109,44 @@ fn solve(
     acc
 }
 
-fn part_1(input: &str) -> usize {
+fn part_1(input: &Vec<Row>) -> usize {
     input
-        .lines()
-        .map(str::trim)
-        .map(|line| {
-            let (known, expected) = line.split_once(' ').unwrap();
-            let known: Vec<Option<bool>> = known
-                .chars()
-                .map(|c| match c {
-                    '.' => Some(false),
-                    '?' => None,
-                    '#' => Some(true),
-                    _ => unreachable!(),
-                })
-                .collect();
-            let expected = expected
-                .split(',')
-                .map(|field| field.parse::<usize>().unwrap())
-                .collect_vec();
-            solve(&known, &expected, 0, 0, &mut HashMap::new())
+        .iter()
+        .map(|row| {
+            let Row {
+                known_cells,
+                expected_chunks,
+            } = row;
+            solve_row(known_cells, expected_chunks, 0, 0, &mut HashMap::new())
         })
         .sum()
 }
 
-fn part_2(input: &str) -> usize {
+fn part_2(input: &Vec<Row>) -> usize {
     input
-        .lines()
-        .map(str::trim)
-        .map(|line| {
-            let (known, expected) = line.split_once(' ').unwrap();
-            let known1: Vec<Option<bool>> = known
-                .chars()
-                .map(|c| match c {
-                    '.' => Some(false),
-                    '?' => None,
-                    '#' => Some(true),
-                    _ => unreachable!(),
-                })
-                .collect();
-            let mut known = std::iter::repeat(known1.iter().copied().chain([None]))
-                .take(5)
-                .flatten()
-                .collect_vec();
-            let _ = known.pop(); // extra separator
-            let expected1 = expected
-                .split(',')
-                .map(|field| field.parse::<usize>().unwrap())
-                .collect_vec();
-            let expected = std::iter::repeat(expected1).take(5).flatten().collect_vec();
-            solve(&known, &expected, 0, 0, &mut HashMap::new())
+        .iter()
+        .map(|row| {
+            let Row {
+                known_cells,
+                expected_chunks,
+            } = row;
+            let mut known_cells2 = Vec::with_capacity(known_cells.len() * 5 + 4);
+            known_cells2.extend_from_slice(known_cells);
+            known_cells2.push(None);
+            known_cells2.extend_from_slice(known_cells);
+            known_cells2.push(None);
+            known_cells2.extend_from_slice(known_cells);
+            known_cells2.push(None);
+            known_cells2.extend_from_slice(known_cells);
+            known_cells2.push(None);
+            known_cells2.extend_from_slice(known_cells);
+            let mut expected_chunks2 = Vec::with_capacity(expected_chunks.len() * 5);
+            expected_chunks2.extend_from_slice(expected_chunks);
+            expected_chunks2.extend_from_slice(expected_chunks);
+            expected_chunks2.extend_from_slice(expected_chunks);
+            expected_chunks2.extend_from_slice(expected_chunks);
+            expected_chunks2.extend_from_slice(expected_chunks);
+            solve_row(&known_cells2, &expected_chunks2, 0, 0, &mut HashMap::new())
         })
         .sum()
 }
@@ -136,9 +154,12 @@ fn part_2(input: &str) -> usize {
 fn main() {
     let session = std::fs::read_to_string(".session.txt").unwrap();
     let session = session.trim();
+    let mut both = PreParsed::new(parse, part_1, part_2);
+    let part_2 = both.part_2();
     if let Err(error) = aoc_magic!(session, 2023:12:2, part_2) {
         eprintln!("Part 2 failed: {error:?}");
     }
+    let part_1 = both.part_1();
     if let Err(error) = aoc_magic!(session, 2023:12:1, part_1) {
         eprintln!("Part 1 failed: {error:?}");
     }
@@ -152,6 +173,9 @@ fn example() {
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1";
-    assert_eq!(part_1(input), 21);
-    assert_eq!(part_2(input), 525152);
+    let mut both = PreParsed::new(parse, part_1, part_2);
+    let part_1 = both.part_1();
+    assert_eq!(part_1(input), &21);
+    let part_2 = both.part_2();
+    assert_eq!(part_2(input), &525152);
 }
