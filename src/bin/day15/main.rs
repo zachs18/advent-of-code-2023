6169d69d87d9164996789dcbe0ff1a737778a2cb
Zachary S <zasample18+github@gmail.com>
2023-12-14 23:24:01 -0600

    Day 15 part 2.

--- src/bin/day15/main.rs
+++ src/bin/day15/main.rs
@@ -1,6 +1,9 @@
 #![allow(unused_imports)]
+use std::collections::HashMap;
+
 use aoc_2023::*;
 use aoc_driver::*;
+use indexmap::IndexMap;
 use itertools::Itertools;
 use zachs18_stdx::*;
 
@@ -17,8 +20,40 @@ fn part_1(input: &str) -> usize {
     input.split(',').map(hash).sum()
 }
 
-fn part_2(input: &str) -> u64 {
-    todo!()
+/// None is remove
+fn parse_lens(s: &str) -> (&str, Option<usize>) {
+    if let Some((label, length)) = s.split_once('=') {
+        (label, Some(length.parse().unwrap()))
+    } else {
+        (&s[..s.len() - 1], None)
+    }
+}
+
+fn part_2(input: &str) -> usize {
+    let values = input.split(',').collect_vec();
+    let mut boxes: Vec<IndexMap<&str, usize>> = vec![IndexMap::new(); 256];
+    for &value in &values {
+        let (label, focal_length) = parse_lens(value);
+        let hash = hash(label);
+        match focal_length {
+            Some(length) => {
+                boxes[hash].insert(label, length);
+            }
+            None => {
+                boxes[hash].shift_remove(label);
+            }
+        }
+    }
+    boxes
+        .iter()
+        .enumerate()
+        .map(|(bxidx, bx)| {
+            bx.iter()
+                .enumerate()
+                .map(|(idx, (_, length))| (bxidx + 1) * (idx + 1) * length)
+                .sum::<usize>()
+        })
+        .sum()
 }
 
 fn main() {
@@ -37,6 +72,6 @@ fn example() {
     let input = "HASH";
     assert_eq!(part_1(input), 52);
     let input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7";
-    assert_eq!(part_1(input), 42);
-    assert_eq!(part_2(input), 42);
+    assert_eq!(part_1(input), 1320);
+    assert_eq!(part_2(input), 145);
 }
